1065adc31e6d9da63829c84d64b806ed0cf0381c
albz007 <albinjoseph229@gmail.com>
2023-08-03 20:58:31 +0530

    a

---
+++ DS/1_1storageclass.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+ int a,b;
+ void display()
+ {
+    int c;
+    printf("%d is a local variable\n",c);
+ }
+ void stat()
+ {
+    static int d=10;
+    printf("%d\n",d);
+    d++;
+ } 
+ void reg()
+ {
+    register int r;
+    printf("%d is a register variable",r);
+
+ }
+ int main()
+ {
+    printf("%d and %d is global variable\n",a,b);
+    display();
+    stat();
+    stat();
+    reg();
+    return 0;
+ }
