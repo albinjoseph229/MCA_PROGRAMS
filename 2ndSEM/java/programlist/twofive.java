1ede1a9a46000eb13f2057f0c14dd500889e291b
Albin Joseph <113278643+albz007@users.noreply.github.com>
2024-01-05 14:33:45 +0530

    Update twofive.java

--- 2ndSEM/java/programlist/twofive.java
+++ 2ndSEM/java/programlist/twofive.java
@@ -1,52 +1,42 @@
-import java.io.*;
-class twofive
-{
-    public static void main(String args[])
-    {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String args[]) {
         System.out.println("Albin Joseph");
-        int arr[][]=new int[10][10];
-        DataInputStream din=new DataInputStream(System.in);
-        int rno,cno,rsum,csum,i,j;
-        try
-        {
-            System.out.println("Enter number of colums");
-            rno=Integer.parseInt(din.readLine());
+        int arr[][] = new int[10][10];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int rno, cno, rsum, csum, i, j;
+        try {
+            System.out.println("Enter the number of columns");
+            cno = Integer.parseInt(reader.readLine());
             System.out.println("Enter the number of rows");
-            cno=Integer.parseInt(din.readLine());
-            for(i=0;i<rno;i++)
-            {
-                for(j=0;j<cno;j++)
-                {
-                    System.out.println("enter the["+i+"]["+j+"] th element");
-                    arr[i][j]=Integer.parseInt(din.readLine());
+            rno = Integer.parseInt(reader.readLine());
+            for (i = 0; i < rno; i++) {
+                for (j = 0; j < cno; j++) {
+                    System.out.println("Enter the [" + i + "][" + j + "] element");
+                    arr[i][j] = Integer.parseInt(reader.readLine());
                 }
             }
-            System.out.println("Rowsum is ");
-            for(i=0;i<rno;i++)
-            {
-                rsum=0;
-                for(j=0;j<cno;j++)
-                {
-                    rsum=rsum+arr[i][j];
-                    System.out.println(""+arr[i][j]);
+            System.out.println("Row sums are:");
+            for (i = 0; i < rno; i++) {
+                rsum = 0;
+                for (j = 0; j < cno; j++) {
+                    rsum = rsum + arr[i][j];
+                    System.out.print(arr[i][j] + " ");
                 }
-                System.out.println("|"+rsum);
+                System.out.println("| " + rsum);
             }
-            System.out.println("Column Sum is");
-            for(i=0;i<cno;i++)
-            {
-                csum=0;
-                for(j=0;j<rno;j++);
-                {
-                    csum=csum+arr[i][j];
-                    System.out.println(""+arr[i][j]);
+            System.out.println("Column sums are:");
+            for (i = 0; i < cno; i++) {
+                csum = 0;
+                for (j = 0; j < rno; j++) {
+                    csum = csum + arr[j][i];
                 }
-                System.out.println("|"+csum);
+                System.out.print(" " + csum);
             }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
         }
-        catch(Exception e)
-        {
-            System.out.println("Error"+e);
-        }  
     }
 }
